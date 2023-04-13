7a0ab28c884476d456d9d528be243f7f561aaa73
Jon Sakas <jon.sakas@community.com>
2023-04-13 11:57:56 -0600

    update mock implementation

--- __mocks__/uuid.ts
+++ __mocks__/uuid.ts
@@ -1,13 +1,1 @@
-import { vi } from 'vitest'
-
-// this is how it was done with jest
-//
-// export const v4 = () => 'generated-id'
-
-vi.mock('uuid', async (importOriginal) => {
-  const original : any = await importOriginal()
-  return {
-    ...original,
-    v4: () => 'generated-id',
-  }
-})
+const v4 = () => 'generated-id'
--- test/uuid.test.ts
+++ test/uuid.test.ts
@@ -1,8 +1,10 @@
-import { v4 } from 'uuid'
-import { describe, expect, it } from 'vitest'
+import { v4 } from 'uuid';
+import { describe, expect, it, vi } from 'vitest';
+
+vi.mock('uuid');
 
 describe('uuid', () => {
   it('returns generated-id mock', () => {
-    expect(v4()).toEqual('generated-id')
-  })
-})
+    expect(v4()).toEqual('generated-id');
+  });
+});
